1a47d52497127de3e6bf7f111b2c52d87ea825a7
winona <w.wrigley@student.unsw.edu.au>
2025-05-04 22:16:54 +1000

    on click handlers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const initialPegState = [
-    ["sml", "md", "lg"],
+    ["r1", "r2", "r3"],
     ["", "", ""],
     ["", "", ""],
   ];
   const [selectedRing, setSelectedRing] = useState(null);
-  const [pegs, setPegs] = useState(initialPegState);
+  const [pegState, setPegState] = useState(initialPegState);
+  const [updatedRing, setUpdatedRing] = useState(false);
+
+  const handleRingClick = (slot, event) => {
+    event.stopPropagation();
+    console.log(slot);
+    setSelectedRing(slot);
+  };
+
+  const handlePegClick = (index) => {
+    if (updatedRing) {
+      console.log("IN");
+      let updatedPegs = [...pegState];
+
+      const newPegs = updatedPegs.map((peg) => {
+        console.log(peg);
+        return peg.map((slot) => {
+          return slot === selectedRing ? "" : slot;
+        });
+      });
+      console.log("OUT");
+      console.log(newPegs);
+    }
+  };
+
+  useEffect(() => {
+    if (selectedRing) {
+      console.log("HERE " + selectedRing);
+      setUpdatedRing(true);
+    }
+  }, [selectedRing]);
 
   return (
     <div className="App">
-      {pegs.map((peg, index) => {
+      {pegState.map((peg, index) => {
         return (
-          <div className="peg" key={`peg${index}`}>
+          <div
+            className="peg"
+            key={`peg${index}`}
+            onClick={() => handlePegClick(index)}>
             {peg.map((slot, index) => {
               return (
                 <div className="slot" key={`slot${index}`}>
-                  {slot !== "" && <div className={`ring ${slot}`}></div>}
+                  {slot !== "" && (
+                    <div
+                      className={`ring ${slot}`}
+                      onClick={(event) => handleRingClick(slot, event)}></div>
+                  )}
                 </div>
               );
             })}
